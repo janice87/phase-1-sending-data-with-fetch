48327aba09a3b322731545342c1b21a752b1ac26
Janice Chan <janice.chan1288@gmail.com>
2022-03-25 16:44:15 -0400

    commit tree 7880e961e0e11daf3bdaa97b3d1f076a614fa415

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail) {
+return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name: "Steve",
+        email: "steve@steve.com"
+    })
+})
+.then(res => res.json())
+.then(data => renderId(data))
+ .catch(error => errorMessage(error))
+}
+
+function renderId(data) {
+    document.body.innerHTML = data.id
+} 
+
+function errorMessage(error){
+document.body.innerHTML = error.message
+}
